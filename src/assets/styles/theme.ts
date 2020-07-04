0870ad3d24a76b85cdc37176c654653e01d57170
Khaled Albarawy <khaled.albarawi@gmail.com>
2020-07-04 18:31:30 +0200

    [FEAT] Added shared theme and wording

---
+++ src/assets/styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    primary: "#3c415e",
+    primary_200: "#3c415e",
+    secondary: "#1cb3c8",
+    secondary_200: "#29cbe0",
+    accent: "#ecf5ff",
+    accent_200: "#738598",
+  },
+};
---
+++ src/shared/i18n/en.wording.ts
@@ -0,0 +1,16 @@
+export const WORDING = {
+  WRITING_PAD: {
+    TEXT_AREA: {
+      ACTIONS: {
+        GENERATE_BTN: "Generate document",
+      },
+      INITIAL_TEXT: `Dear Lauren and Steve,
+  
+      Thank you so much for the thoughtful wedding gift. We were looking at crystal wine glasses just 
+      the other week.
+        
+All the best,
+Allison and Jeremy`,
+    },
+  },
+};
