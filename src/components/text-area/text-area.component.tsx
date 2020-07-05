215f56379e386bf0c3b585ca17da96be5d596dc0
Khaled Albarawy <khaled.albarawi@gmail.com>
2020-07-05 18:00:34 +0200

    [FEAT] Added aria-label to identify textarea

--- src/components/text-area/text-area.component.tsx
+++ src/components/text-area/text-area.component.tsx
@@ -31,6 +31,7 @@ export const TextArea: React.FC<TextAreaProps> = ({ text, onChange }) => {
       value={text}
       onChange={onChange}
       placeholder={t.PLACEHOLDER}
+      aria-label={t.PLACEHOLDER}
     />
   );
 };
