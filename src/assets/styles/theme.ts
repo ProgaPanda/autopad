e6830fae310b479cb2033ef79076e2a5e33eb68d
Khaled Albarawy <khaled.albarawi@gmail.com>
2020-07-05 18:48:08 +0200

    [MAINTENANCE] Sync. theme props

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -1,10 +1,10 @@
 export const theme = {
   colors: {
     primary: "#3c415e",
-    primary_200: "#3c415e",
     secondary: "#1cb3c8",
     secondary_200: "#29cbe0",
     accent: "#d3e8ff",
     accent_200: "#ecf5ff",
+    accent_400: "#738598",
   },
 };
