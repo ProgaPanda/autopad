473a1498b80d5b85527ae6f487420b828687d8d5
Khaled Albarawy <khaled.albarawi@gmail.com>
2020-07-09 23:44:21 +0200

    [FEAT] remove number arrow from input component

--- src/components/input/input.component.tsx
+++ src/components/input/input.component.tsx
@@ -2,24 +2,6 @@ import React from "react";
 import styled, { css } from "styled-components";
 import { theme } from "../../assets/styles/theme";
 
-const disabledPartial = css`
-  background: ${theme.colors.neutral_600};
-  :hover {
-    cursor: not-allowed;
-  }
-`;
-
-const StyledInputContainer = styled.div`
-  display: flex;
-  min-width: 100px;
-  border: 2px solid ${theme.colors.accent};
-  border-radius: 5px;
-
-  :focus-within {
-    border-color: ${theme.colors.secondary};
-  }
-`;
-
 const StyledInput = styled.input<InputProps>`
   padding: 1rem 1.5rem;
   border: 0;
@@ -33,7 +15,36 @@ const StyledInput = styled.input<InputProps>`
     outline: 0;
   }
 
+  /* Remove number arrows from input type number */
+  ::-webkit-outer-spin-button,
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  [type="number"] {
+    -moz-appearance: textfield;
+  }
+
   ${(props: InputProps) => props.disabled && disabledPartial}
+`;
+
+const StyledInputContainer = styled.div`
+  display: flex;
+  min-width: 100px;
+  border: 2px solid ${theme.colors.accent};
+  border-radius: 5px;
+
+  :focus-within {
+    border-color: ${theme.colors.secondary};
+  }
+`;
+
+const disabledPartial = css`
+  background: ${theme.colors.neutral_600};
+  :hover {
+    cursor: not-allowed;
+  }
 `;
 
 const StyledPrefix = styled.div`
